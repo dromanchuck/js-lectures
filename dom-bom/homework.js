536f6593b08d9d684af0106585634bcbd1681488
Dmitry <d.ramanchuk@arateg.com>
2021-05-07 15:07:54 +0300

    Add quiz app

---
+++ dom-bom/homework.js
@@ -0,0 +1,48 @@
+et buttonAdd = document.createElement("button");
+buttonAdd.innerHTML = "Добавить вопрос";
+// buttonAdd.disabled = true; задизэйблить кнопку
+
+let questions = [];
+
+buttonAdd.addEventListener("click", () => {
+  let question = prompt("Введите текст вопроса:");
+
+  let answers = new Array(4).fill("").map((_el, index) => {
+    let answer = prompt(`Введите вариант ответа ${index + 1}`);
+
+    return answer;
+  });
+
+  let correctAnswers = prompt("Введите через запятую правильные варианты");
+
+  let correctAnswersArr = correctAnswers
+    .split(",")
+    .map((value) => Number(value) - 1);
+
+  let correctAnswersSet = new Set(correctAnswersArr);
+
+  let answersArr = answers.map((answer, index) => ({
+    text: answer,
+    isCorrect: correctAnswersSet.has(index),
+  }));
+
+  console.log({ answersArr });
+
+  let questionObj = {
+    question,
+    answers: answersArr,
+  };
+
+  questions.push(questionObj);
+
+  if (!question) {
+    alert("Вы не ввели текст вопроса, попробуйте добавить вопрос еще раз");
+  }
+});
+
+let buttonBeginTest = document.createElement("button");
+let textNode = document.createTextNode("Начать тест");
+
+buttonBeginTest.append(textNode);
+
+document.body.append(buttonAdd, buttonBeginTest);
---
+++ dom-bom/useful-links.md
@@ -0,0 +1,6 @@
+### Полезные ссылки
+
+- [Документ](https://learn.javascript.ru/document)
+- [События](https://learn.javascript.ru/events)
+- [Интерфейсные события](https://learn.javascript.ru/event-details)
+- [Формы](https://learn.javascript.ru/forms-controls)
