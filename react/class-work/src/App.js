0890279d9ea26a9ff17d5a794e39389e5ae8e530
Dmitry <d.ramanchuk@arateg.com>
2021-09-17 11:13:09 +0300

    Add class work

--- react/class-work/src/App.js
+++ react/class-work/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { TweetCard } from "./components/TweetCard";
 import "./App.css";
 import { Counter } from "./components/Counter";
+import { Button } from "./components/Button";
+import { Time } from "./Time";
 
 const tweets = [
   {
@@ -10,8 +12,7 @@ const tweets = [
     nickName: "@dan_abramov",
     date: "7 Sep",
     post: "asdfasdfasdfasdf",
-    avatarUrl:
-      "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
+    avatarUrl: "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
 
     replyCount: 12,
     retweetCount: 10,
@@ -22,8 +23,7 @@ const tweets = [
     nickName: "@dan_abramov",
     date: "7 Sep",
     post: "Woke up from a vivid dream about the people from the first team i worked on. As if we’re working on something else now. I was the youngest, and there was this sense… like i want to earn their respect. Like hey, look. I can do this too. See? And they’re like yea, you’re right.",
-    avatarUrl:
-      "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
+    avatarUrl: "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
 
     replyCount: 12,
     retweetCount: 10,
@@ -34,8 +34,7 @@ const tweets = [
     nickName: "@dan_abramov",
     date: "7 Sep",
     post: "ashdfklahskldfhakljsdhflkjhasdkl",
-    avatarUrl:
-      "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
+    avatarUrl: "https://static.toiimg.com/thumb/msid-67586673,widt…de-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg",
 
     replyCount: 12,
     retweetCount: 10,
@@ -46,7 +45,12 @@ const tweets = [
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Button text={"primary"} type={"primary"} onClick={() => alert("Primary")} />
+      <Button text={"secondary"} type={"secondary"} onClick={() => alert("Secondary")} />
+      <Button text={"text"} type={"text"} onClick={() => alert("Text")} />
+      {/* <Counter background={"red"} initialCount={5} />
+      <Counter background={"yellow"} />
+      <Counter background={"green"} /> */}
       {/* {tweets.map(
         (tweet, index) => {
           const {
--- react/class-work/src/components/Counter/Counter.jsx
+++ react/class-work/src/components/Counter/Counter.jsx
@@ -3,10 +3,20 @@ import React from "react";
 export class Counter extends React.Component {
   constructor(props) {
     super(props);
-
+    console.log("constructor ", props.background);
     this.state = {
-      count: 10,
+      count: props.initialCount || 0,
     };
+
+    this.onClickMinus = this.onClickMinus.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount ", this.props.background);
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate ", this.props.background);
   }
 
   onClickPlus = () => {
@@ -16,40 +26,52 @@ export class Counter extends React.Component {
   };
 
   onClickMinus() {
-    alert("-");
+    this.setState({
+      count: --this.state.count,
+    });
   }
 
   render() {
+    const { count } = this.state;
+    const background = this.props.background;
+    console.log("render ", this.props.background);
     return (
       <div
         style={{
           display: "flex",
           alignItems: "center",
+          background,
         }}
       >
-        <button
-          style={{
-            fontSize: 100,
-          }}
-          onClick={this.onClickMinus}
-        >
-          -
-        </button>
+        {count !== 0 && (
+          <button
+            style={{
+              fontSize: 100,
+            }}
+            onClick={this.onClickMinus}
+          >
+            -
+          </button>
+        )}
+
         <p
           style={{
             fontSize: 100,
           }}
         >
-          {this.state.count}
+          {count}
         </p>
-        <button
-          style={{
-            fontSize: 100,
-          }}
-          onClick={this.onClickPlus}
-        >
-          +
-        </button>
+        {count === 10 ? null : (
+          <button
+            style={{
+              fontSize: 100,
+            }}
+            onClick={this.onClickPlus}
+          >
+            +
+          </button>
+        )}
+        {count === 10 ? <p>Убери свои грязные руки!</p> : null}
       </div>
     );
   }
